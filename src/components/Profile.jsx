ecb08590b8f95fbd674e9114bec7067550af7dc5
pablomdh <pablodubh@gmail.com>
2021-08-12 20:09:46 -0300

    follow

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,133 +7,124 @@ import RightPanel from "./RightPanel";
 import Tweet from "./Tweet";
 
 const Profile = () => {
-	const dispatch = useDispatch();
-	const { loggedUser } = useSelector((state) => state.tweetReducer);
-	const { token } = useSelector((state) => state.authReducer);
-	const { username } = useParams();
+  const dispatch = useDispatch();
+  const { loggedUser } = useSelector((state) => state.tweetReducer);
+  const { token } = useSelector((state) => state.authReducer);
+  const { username } = useParams();
 
-	const [profile, setProfile] = useState({});
-	const [tweet, setTweet] = useState([]);
-	const [following, setFollowing] = useState(false);
-	// const [newfollowing, newSetFollowing] = useState("");
+  const [profile, setProfile] = useState({});
+  const [tweet, setTweet] = useState([]);
+  const [following, setFollowing] = useState(false);
+  const [followingCount, setFollowingCount] = useState(0);
+  const [followersCount, setFollowersCount] = useState(0);
+  // const [newfollowing, newSetFollowing] = useState("");
 
-	useEffect(() => {
-		async function api() {
-			const response = await fetch(`http://localhost:3001/${username}`, {
-				method: "GET",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			const { user, tweets } = await response.json();
-			setProfile(user);
-			setTweet(tweets);
-			setFollowing(
-				loggedUser.following.some((arrVal) => profile._id === arrVal.toString())
-			);
-		}
-		api();
-	}, [username, tweet]);
+  useEffect(() => {
+    async function api() {
+      const response = await fetch(`http://localhost:3001/${username}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const { user, tweets } = await response.json();
+      setProfile(user);
+      setTweet(tweets);
+      setFollowing(loggedUser.following.some((arrVal) => user._id === arrVal));
+      setFollowingCount(user.followingCount);
+      setFollowersCount(user.followersCount);
+    }
+    api();
+  }, []);
 
-	useEffect(() => {
-		async function apiFollow() {
-			try {
-				const data = {
-					id: loggedUser._id,
-					following: loggedUser.following,
-					token: loggedUser.token,
-				};
-				await fetch(
-					`http://localhost:3001/${following ? "follow" : "unfollow"}/${
-						profile._id
-					}`,
-					{
-						method: "POST",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-							Authorization: `${token}`,
-						},
-						body: JSON.stringify(data),
-					}
-				);
+  async function handleFollow() {
+    setFollowing((prev) => !prev);
+    if (!following) {
+      setFollowersCount((prev) => prev + 1);
+    } else {
+      setFollowersCount((prev) => prev - 1);
+    }
+    try {
+      await fetch(`http://localhost:3001/${following ? "unfollow" : "follow"}/${profile._id}`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const action = following ? "SET_UNFOLLOW" : "SET_FOLLOW";
+      dispatch({
+        type: `${action}`,
+        payload: profile._id,
+      });
+    } catch {
+      return alert("Error en follow");
+    }
+  }
 
-				const action = following ? "SET_FOLLOW" : "SET_UNFOLLOW";
-				dispatch({
-					type: `${action}`,
-					payload: profile._id,
-				});
-			} catch {
-				return alert("Error en follow");
-			}
-		}
-		apiFollow();
-	}, [following]);
-
-	/*    async function handlerUnfollow() {
+  /*    async function handlerUnfollow() {
         cambiar el boton a unfollow
     } */
 
-	return (
-		<div>
-			<div className="row">
-				<div className="col-3">
-					<LeftPanel />
-				</div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col-3">
+          <LeftPanel />
+        </div>
 
-				{/* <!--middle block--> */}
-				<div className="col-lg-6 col-md-11 col-10">
-					<main>
-						<div className="first-part">
-							<div>
-								<Link className="home">Home</Link>
-							</div>
-							<div>
-								<i className="far fa-star"></i>
-							</div>
-						</div>
-						<div className="whats-happening">
-							<div
-								className="post-blocks"
-								style={{
-									backgroundImage: `url(${profile.backgroundPhoto})`,
-									objectPosition: " 10% 50%",
-								}}
-							>
-								<div className="post-profile">
-									<p>BackgroundPhoto</p>
-								</div>
-								<div className="post-profile">
-									<img
-										src={`${profile.photo}`}
-										className="avatar"
-										alt="Avatar"
-									/>
-									<span>{profile.username}</span>
-								</div>
-								{/* {loggedUser && loggedUser.user.username !== username} */}
-								{profile._id !== loggedUser._id &&
-									(!following ? (
-										<button
-											onClick={() => {
-												setFollowing((prev) => !prev);
-											}}
-										>
-											Follow
-										</button>
-									) : (
-										<button
-											onClick={() => {
-												setFollowing((prev) => !prev);
-											}}
-										>
-											Unfollow
-										</button>
-									))}
-							</div>
-							{/* <div className="post-icons">
+        {/* <!--middle block--> */}
+        <div className="col-lg-6 col-md-11 col-10">
+          <main>
+            <div className="first-part">
+              <div>
+                <Link className="home">Home</Link>
+              </div>
+              <div>
+                <i className="far fa-star"></i>
+              </div>
+            </div>
+            <div className="whats-happening">
+              <div
+                className="post-blocks"
+                style={{
+                  backgroundImage: `url(${profile.backgroundPhoto})`,
+                  objectPosition: " 10% 50%",
+                }}
+              >
+                <div className="post-profile">
+                  <p>BackgroundPhoto</p>
+                </div>
+                <div className="post-profile">
+                  <img src={`${profile.photo}`} className="avatar" alt="Avatar" />
+                  <span>{profile.username}</span>
+                  <span>Following: {followingCount}</span>
+                  <span>Followers:{followersCount}</span>
+                </div>
+                {/* {loggedUser && loggedUser.user.username !== username} */}
+                {profile._id !== loggedUser._id &&
+                  (!following ? (
+                    <button
+                      onClick={() => {
+                        handleFollow();
+                      }}
+                    >
+                      Follow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        handleFollow();
+                      }}
+                    >
+                      Unfollow
+                    </button>
+                  ))}
+              </div>
+              {/* <div className="post-icons">
 								<div className="second-post-icons">
 									<i className="far fa-circle"></i>
 									<i className="fas fa-plus-circle"></i>
@@ -142,25 +133,25 @@ const Profile = () => {
 									</Link>
 								</div>
 							</div> */}
-							{/* <form action="/create" method="POST"></form>*/}
-						</div>
-					</main>
+              {/* <form action="/create" method="POST"></form>*/}
+            </div>
+          </main>
 
-					{/* <!--TWEEEEEETS-->  */}
-					<section id="tweets">
-						{/* <!--1 tweet--> */}
-						{tweet.map((tweet) => {
-							return <Tweet tweet={tweet} setTweet={setTweet} />;
-						})}
-						{/* <!--1 tweet--> }*/}
-					</section>
-				</div>
-				<div className="col-3">
-					<RightPanel />
-				</div>
-			</div>
-		</div>
-	);
+          {/* <!--TWEEEEEETS-->  */}
+          <section id="tweets">
+            {/* <!--1 tweet--> */}
+            {tweet.map((tweet) => {
+              return <Tweet tweet={tweet} setTweet={setTweet} />;
+            })}
+            {/* <!--1 tweet--> }*/}
+          </section>
+        </div>
+        <div className="col-3">
+          <RightPanel />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,150 +2,133 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 import { Link, useParams } from "react-router-dom";
-// import "./index.css";
+import "./index.css";
 
 function Tweet({ tweet, setTweet }) {
-	const { loggedUser } = useSelector((state) => state.tweetReducer);
-	const { token } = useSelector((state) => state.authReducer);
+  const { loggedUser } = useSelector((state) => state.tweetReducer);
+  const { token } = useSelector((state) => state.authReducer);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const [like, setLike] = useState(
-		loggedUser.tweetsLiked.some((arrVal) => tweet._id === arrVal.toString())
-	);
-	const [countLike, setCountLike] = useState(tweet.likes);
-	const [tweetId, setTweetId] = useState("");
-	// const [deleteTweet, setDeleteTweet] = useState(false);
-	async function handleDelete() {
-		try {
-			const data = {
-				_id: tweet._id,
-				token: token,
-			};
-			console.log(data);
-			await fetch(`${process.env.REACT_APP_API_URL}/tweet`, {
-				method: "DELETE",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-				body: JSON.stringify(data),
-			});
-			console.log("hizo fetch");
+  const [like, setLike] = useState(loggedUser.tweetsLiked.some((arrVal) => tweet._id === arrVal.toString()));
+  const [countLike, setCountLike] = useState(tweet.likes);
+  const [tweetId, setTweetId] = useState("");
+  // const [deleteTweet, setDeleteTweet] = useState(false);
+  async function handleDelete() {
+    try {
+      const data = {
+        _id: tweet._id,
+        token: token,
+      };
+      console.log(data);
+      await fetch(`${process.env.REACT_APP_API_URL}/tweet`, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+      console.log("hizo fetch");
 
-			dispatch({
-				type: "TWEET_DELETE",
-				payload: tweet._id,
-			});
-		} catch {
-			return alert("Algo salio mal en Delete");
-		}
-		// setTweet(tweet.filter((item) => tweet._id !== item._id));
-	}
+      dispatch({
+        type: "TWEET_DELETE",
+        payload: tweet._id,
+      });
+    } catch {
+      return alert("Algo salio mal en Delete");
+    }
+    // setTweet(tweet.filter((item) => tweet._id !== item._id));
+  }
 
-	function handleLike() {
-		setCountLike((prev) => (!like ? prev + 1 : prev - 1));
-		async function apiLike() {
-			try {
-				const data = {
-					_id: loggedUser._id,
-					token: loggedUser.token,
-				};
-				await fetch(
-					`${process.env.REACT_APP_API_URL}/${
-						like ? "like" : "unlike"
-					}/${tweetId}`,
-					{
-						method: "POST",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-							Authorization: `Bearer ${token}`,
-						},
-						body: JSON.stringify(data),
-					}
-				);
+  function handleLike() {
+    setCountLike((prev) => (!like ? prev + 1 : prev - 1));
+    async function apiLike() {
+      try {
+        const data = {
+          _id: loggedUser._id,
+          token: loggedUser.token,
+        };
+        await fetch(`${process.env.REACT_APP_API_URL}/${like ? "like" : "unlike"}/${tweetId}`, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        });
 
-				const action = like ? "SET_LIKE" : "SET_UNLIKE";
-				dispatch({
-					type: `${action}`,
-					payload: tweetId,
-				});
-			} catch {
-				return alert("Algo salio mal en Like/UnLike");
-			}
-		}
-		apiLike();
-	}
+        const action = like ? "SET_LIKE" : "SET_UNLIKE";
+        dispatch({
+          type: `${action}`,
+          payload: tweetId,
+        });
+      } catch {
+        return alert("Algo salio mal en Like/UnLike");
+      }
+    }
+    apiLike();
+  }
 
-	return (
-		<div key={tweet._id}>
-			<div className="tweet-1">
-				<div className="tweet-img">
-					<img src={`${tweet.author.photo}`} alt="Avatar" />
-				</div>
-				<div className="tweet-txt">
-					<div className="tweet-name-date">
-						<Link to={`/${tweet.author.username}`}>
-							<strong> {tweet.author.firstname}</strong>
-							<span className="twitter-account">
-								{" "}
-								@{tweet.author.username}
-							</span>
-						</Link>
-						-
-						<span className="date">
-							{!moment(tweet.date).isBefore(moment().startOf("day"))
-								? moment(tweet.date, "YYYYMMDD").fromNow()
-								: moment(tweet.date, "YYYYMMDD").format("MMM Do YY")}
-						</span>
-					</div>
-					<div className="message">{tweet.content}</div>
-					<div className="tweet-icons d-flex justify-content-between px-2">
-						<div>
-							<i className="far fa-comment"></i>
-							<i className="fas fa-retweet"></i>
-							{!loggedUser.tweetsLiked.some(
-								(arrVal) => tweet._id === arrVal.toString()
-							) ? (
-								<button
-									onClick={() => {
-										setTweetId(tweet._id);
-										setLike((prev) => !prev);
-										handleLike();
-									}}
-								>
-									<i className="far fa-heart">{countLike}</i>
-								</button>
-							) : (
-								<button
-									onClick={() => {
-										setTweetId(tweet._id);
-										setLike((prev) => !prev);
-										handleLike();
-									}}
-								>
-									<i className="fas fa-heart">{countLike}</i>
-								</button>
-							)}
-							<i className="fas fa-external-link-alt"></i>
-						</div>
-						{tweet.author._id === loggedUser._id && (
-							<button
-								onClick={() => {
-									// setDeleteTweet(true);
-									handleDelete();
-								}}
-							>
-								<i class="fas fa-trash-alt"></i>
-							</button>
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div key={tweet._id}>
+      <div className="row tweet-1">
+        <div className="post__avatar col-2">
+          <img className="" src={`${tweet.author.photo}`} alt="Avatar" />
+        </div>
+        <div className="tweet-txt col-10">
+          <div className="tweet-name-date">
+            <Link to={`/${tweet.author.username}`}>
+              <strong> {tweet.author.firstname}</strong>
+              <span className="twitter-account"> @{tweet.author.username}</span>
+            </Link>
+            -<span className="date">{!moment(tweet.date).isBefore(moment().startOf("day")) ? moment(tweet.date, "YYYYMMDD").fromNow() : moment(tweet.date, "YYYYMMDD").format("MMM Do YY")}</span>
+          </div>
+          <div className="message">{tweet.content}</div>
+          <div className="tweet-icons d-flex justify-content-between px-2">
+            <div>
+              <i className="far fa-comment"></i>
+              <i className="fas fa-retweet"></i>
+              {!loggedUser.tweetsLiked.some((arrVal) => tweet._id === arrVal.toString()) ? (
+                <button
+                  onClick={() => {
+                    setTweetId(tweet._id);
+                    setLike((prev) => !prev);
+                    handleLike();
+                  }}
+                >
+                  <i className="far fa-heart">{countLike}</i>
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    setTweetId(tweet._id);
+                    setLike((prev) => !prev);
+                    handleLike();
+                  }}
+                >
+                  <i className="fas fa-heart">{countLike}</i>
+                </button>
+              )}
+              <i className="fas fa-external-link-alt"></i>
+            </div>
+            {tweet.author._id === loggedUser._id && (
+              <button
+                onClick={() => {
+                  // setDeleteTweet(true);
+                  handleDelete();
+                }}
+              >
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tweet;
